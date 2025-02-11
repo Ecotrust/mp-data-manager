65e2146bbcc508cded58126f6bcb23f2ce176a44
David Pollard <pollardld@gmail.com>
2025-02-10 16:22:14 -0800

    Add Refresh button to each layer

--- data_manager/static/admin/js/layer_http_status.js
+++ data_manager/static/admin/js/layer_http_status.js
@@ -4,10 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const url = el.getAttribute('data-url');
         if (url) {
             fetchStatus(url).then(status => {
-                const statusText = status === 200 ? `OK ${status}` : `Fail ${status}`;
-                el.textContent = statusText;
-                el.classList.add(status === 200 ? 'success' : 'fail');
+                addStatus(el, status);
             });
+            addRefreshButton(el, url);
         }
     });
 });
@@ -20,4 +19,41 @@ async function fetchStatus(url) {
         console.error('Fetch error:', error);
         return 404;
     }
+}
+
+function addStatus(el, status) {
+    const statusText = status === 200 ? `OK ${status}` : `Fail ${status}`;
+    el.textContent = statusText;
+    el.classList.add(status === 200 ? 'success' : 'fail');
+}
+
+function addRefreshButton(el, url) {
+    let refreshLink = document.createElement("a");
+    refreshLink.href = url;
+    refreshLink.title = "Refresh Statuses";
+    refreshLink.style.display = "block";
+    refreshLink.style.width = "40px";
+    refreshLink.style.height = "40px";
+    refreshLink.innerHTML = `
+        <svg width="24" height="24" viewBox="0 0 24 24">
+            <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 
+                     0 .46-.05.91-.14 1.34l1.46 1.46C19.48 13.16 
+                     20 12.12 20 11c0-4.42-3.58-8-8-8zm-6.36.64L4.22 
+                     6.04C3.87 6.53 3.67 7.12 3.67 7.75c0 4.42 3.58 8 
+                     8 8v3l4-4-4-4v3c-3.31 0-6-2.69-6-6 0-.63.2-1.22.56-1.71z" 
+                  fill="#000"/>
+        </svg>
+    `;
+    refreshLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        el.classList.add("refreshing");
+        el.textContent = "Refreshing...";
+        fetchStatus(url).then(status => {
+            addStatus(el, status);
+        }).finally(() => {
+            el.classList.remove("refreshing");
+        });
+    });
+    el.parentNode.appendChild(refreshLink);
+    console.log(el)
 }
